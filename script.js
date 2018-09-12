82535ba7447638c22d68ae61e0224e360935d7a1
kmyles82 <kmyles82@gmail.com>
2018-09-12 12:47:27 -0500

    added function to create new object and display to webpage

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-var g = Greetr('Kerry', 'Myles');
-console.log(g);
+var g = G$('Kerry', 'Myles');
+//console.log(g);
+//chainable
+g.greet().setLang('es').greet(true).log();
+
+$('#login').click(function(){
+    let loginGrtr = G$('Kerry', 'Myles');
+    
+    $('#logindiv').hide();
+    
+    loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+    
+    
+})
